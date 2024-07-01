20527aca3b6128aa7a31931f2e7e1bd7f3113fb0
A.Arounesh <110887451+hsenuora@users.noreply.github.com>
2024-07-01 10:14:13 +0530

    four

---
+++ jfourthprogram/src/jfourthprogram/identifiers.java
@@ -0,0 +1,16 @@
+package jfourthprogram;
+
+public class identifiers {
+	public static void main(String args[]) {
+		int number=13;
+		System.out.println("value of the number varible is:"+number);
+	int number1=13;
+	System.out.println("value of the number variable is:"+number1);
+	int number2=10;
+	int number3=20;
+	System.out.println("value of the number variable is:"+number3);
+	String studentName="Ankiet";
+	System.out.println("value:"+studentName);
+	
+	}
+}
